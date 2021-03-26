b6d377a4db10b68844996d3758c636db968e45f0
aminebirani <a.birani.elhbak@gmail.com>
2021-03-26 14:26:10 +0100

    feat(resource): Add Coupon resource to api

---
+++ src/qrcode/coupon/coupon.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CouponService } from './coupon.service';
+import { CreateCouponDto } from './dto/create-coupon.dto';
+import { UpdateCouponDto } from './dto/update-coupon.dto';
+
+@Controller('coupon')
+export class CouponController {
+  constructor(private readonly couponService: CouponService) {}
+
+  @Post()
+  create(@Body() createCouponDto: CreateCouponDto) {
+    return this.couponService.create(createCouponDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.couponService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.couponService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCouponDto: UpdateCouponDto) {
+    return this.couponService.update(+id, updateCouponDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.couponService.remove(+id);
+  }
+}
